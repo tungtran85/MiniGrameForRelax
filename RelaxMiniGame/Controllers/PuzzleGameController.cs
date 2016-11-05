6737a7adb5d377a782c81ee68d848eaa5bc5ce1a
Tran Thanh Tung <thanhtung12a32003@gmail.com>
2016-11-06 01:04:58 +0700

    init puzzle game

---
+++ RelaxMiniGame/Controllers/PuzzleGameController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RelaxMiniGame.Controllers
+{
+    public class PuzzleGameController : Controller
+    {
+        // GET: PuzzleGame
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
