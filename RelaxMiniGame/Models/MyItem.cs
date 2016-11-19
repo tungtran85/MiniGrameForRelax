3d1f35dd72ad34ed5931d1530ebf143c03e77854
Tran Thanh Tung <thanhtung12a32003@gmail.com>
2016-11-20 02:08:24 +0700

    swap and counter

---
+++ RelaxMiniGame/Models/MyItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RelaxMiniGame.Models
+{
+    public class MyItem
+    {
+        public string Mykey { get; set; }
+        public string Myvalue { get; set; }
+    }
+}
---
+++ RelaxMiniGame/Models/VietlottVNViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RelaxMiniGame.Models
+{
+    public class VietlottVNViewModel
+    {
+        public List<int> ListFrequencyNumbers { get; set; }
+
+        public VietlottVNViewModel()
+        {
+            ListFrequencyNumbers = new List<int>();
+        }
+
+    }
+}
